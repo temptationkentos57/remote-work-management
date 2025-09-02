feface6a011ab8b1fecbd8adfe89841742b1550b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-02 18:28:27 +0200

    Add logging middleware to track requests.

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/tasks', (req, res) => {
   res.json([{ id: 1, title: 'Task 1', completed: false }]);
 });
