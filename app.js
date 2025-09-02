86dfdcbe97323c0076454e4002d7031ede7fab64
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-02 18:30:54 +0200

    Add a response handler for 404 Not Found errors

--- app.js
+++ app.js
@@ -15,5 +15,10 @@ app.get('/tasks', (req, res) => {
   res.json({ status: 'success', data: [{ id: 1, title: 'Task 1', completed: false }] });
 });
 
+// 404 Not Found handler
+app.use((req, res) => {
+  res.status(404).json({ status: 'error', message: 'Not Found' });
+});
+
 // Export the Express app
 module.exports = app;
