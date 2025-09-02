1a0a561537bdfe5689a1e0bc38492ce5ec2650b9
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-02 18:27:48 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Ứng dụng Quản Lý Công Việc Từ Xa!');
+});
+
+app.listen(port, () => {
+  console.log(`Ứng dụng đang chạy trên http://localhost:${port}`);
+});
